6778e37f936d258dc43c8f113aea4f430133e766
Joonas Kuorilehto <joneskoo@derbian.fi>
2016-02-06 16:18:10 +0200

    Move CSS to a static file

--- cmd/shorturl-server/serve.go
+++ cmd/shorturl-server/serve.go
@@ -23,14 +23,16 @@ func main() {
 	addr := "[::1]:8000"
 	log.Print("Listening on", addr)
 	base := views.NewView(contentRoot, db)
-	http.HandleFunc("/",
-		func(w http.ResponseWriter, req *http.Request) {
-			if req.URL.Path != "/" {
-				views.RedirectView{base}.ServeHTTP(w, req)
-				return
-			}
-			views.IndexView{base}.ServeHTTP(w, req)
-		})
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		// Redirect yx.fi/xxx to target
+		if req.URL.Path != "/" {
+			views.RedirectView{base}.ServeHTTP(w, req)
+			return
+		}
+		// For root URI /, serve index page
+		views.IndexView{base}.ServeHTTP(w, req)
+	})
 	http.Handle("/p/", http.StripPrefix("/p", views.PreviewView{base}))
+	http.Handle("/static/", http.FileServer(http.Dir(contentRoot)))
 	http.ListenAndServe(addr, nil)
 }
